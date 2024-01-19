fa7b7e0019a353b27a3103e78558970697dd3f57
sardorml <sardormamadaliyev@gmail.com>
2024-01-19 12:07:48 +0900

    Update navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,8 +2,8 @@ import NavElement from "./NavElement";
 
 export default function Navigation() {
   return (
-    <header className="mx-auto max-w-7xl xl:px-8 ">
-      <div className="flex justify-between py-5">
+    <header className="mx-auto max-w-7xl xl:px-8 sticky top-0 bg-zinc-50 bg-zinc-50/50 backdrop-blur backdrop-filter z-[10]">
+      <div className="flex justify-between py-5 border-b border-slate-300">
         <h1 className="text-4xl font-bold text-slate-700 flex items-center">
           Defilens
         </h1>
