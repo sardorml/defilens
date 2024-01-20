924e3f8d74999f8ae9fb59e673b8da0d385f0a60
sardorml <sardormamadaliyev@gmail.com>
2024-01-21 02:16:06 +0900

    Add skeleton

---
+++ src/components/skeleton/SkeletonList.tsx
@@ -0,0 +1,7 @@
+export default function SkeletonList() {
+  return (
+    <div className="animate-pulse">
+      <div className="h-[52px] bg-gray-100 rounded-lg dark:bg-gray-300 mb-4"></div>
+    </div>
+  );
+}
